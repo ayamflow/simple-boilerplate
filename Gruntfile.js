2fc3280bbfcf6ddf7ec2d6d0f7d7195468b897ae
Florian Morel <florian@hellohikimori.com>
2013-12-05 15:02:44 +0100

    Initial commit.

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        sass: {
+            dist: {
+                options: {
+                    style: 'expanded',
+                    loadPath: 'sass'
+                },
+                files: {
+                    'css/styles.css' : 'sass/styles.scss'
+                }
+            }
+        },
+        watch: {
+            css: {
+                files: 'sass/*.scss',
+                tasks: ['sass']
+            }
+        },
+         connect: {
+            server: {
+                options: {
+                    port: 9734,
+                    open: true,
+                    keepalive: true
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+
+    grunt.registerTask('default', ['sass', 'watch']);
+};
---
+++ README.md
@@ -0,0 +1,6 @@
+# No framework boilerplate
+
+A simple boilerplate I use for simple sites, tech demos, etc.
+When I don't use any JS Framework.
+
+I use Bourbon, Neat, and normalize.scss.
